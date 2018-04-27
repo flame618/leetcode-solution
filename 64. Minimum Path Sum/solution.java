c6eb48c0c00a2459b82da4042cac7cec15fe2aec
MCProject <859057040@qq.com>
2018-04-27 17:53:00 +0800

    Create solution.java

---
+++ 64. Minimum Path Sum/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    /*
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(i == 0 && j == 0) continue;
+                else if(i == 0) dp[i][j] = dp[i][j - 1] + grid[i][j];
+                else if(j == 0) dp[i][j] = dp[i - 1][j] + grid[i][j];
+                else dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+    */
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < m; i++) dp[i][0] = dp[i - 1][0] + grid[i][0];
+        for(int i = 1; i < n; i++) dp[0][i] = dp[0][i - 1] + grid[0][i];
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
