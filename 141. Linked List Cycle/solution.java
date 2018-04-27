ac7f4bb3e4097402f1175aee71822b05131448c5
MCProject <859057040@qq.com>
2018-04-27 18:13:21 +0800

    Create solution.java

---
+++ 141. Linked List Cycle/solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode>nodes = new HashSet<ListNode>();
+        while(head != null)
+        {
+            if(nodes.contains(head))
+            {
+                return true;
+            }
+            nodes.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
