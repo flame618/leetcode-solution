2b1711d5142acb34f7107d86dfec8a4d99545346
MCProject <859057040@qq.com>
2018-04-27 17:53:34 +0800

    Create solution.java

---
+++ 66. Plus One/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int l = digits.length;
+        for(int i = l - 1; i >= 0; i--)
+        {
+            if(digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] res = new int[l + 1];
+        res[0] = 1;
+        return res;
+    }
+}
